f0d78f30ef24466cba0f607f5108803e075e2748
godc03 <yx.godot@gmail.com>
2016-10-05 11:32:46 +0800

    ver 1

---
+++ Assets/demo0/GameLogic.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameLogic : MonoBehaviour {
+
+	public float MoveInterval = 3.7f;	//0.7s per Cell
+	public float Map1BeginX = -3.085f;
+	public float Map1BeginY = -3.065f;
+	public float CellSize =  0.317f;	
+	public float Map2BeginX = 0.169f;
+	public float Map2BeginY = -3.065f;
+	public int MaxX = 9;
+	public int MaxY = 19;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/demo1/Client.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Client : MonoBehaviour {
+
+	string IP = "127.0.0.1";
+	int Port = 2016;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnGUI(){
+		switch (Network.peerType) {
+		case NetworkPeerType.Disconnected:
+			StartConnect();
+			break;
+		case NetworkPeerType.Client:
+			InGame();
+			break;
+		default:
+			break;
+		}
+	}
+
+	void StartConnect()
+	{
+		if (GUILayout.Button ("Login")) {
+			NetworkConnectionError err = Network.Connect(IP,Port);
+			Debug.Log ("Error Msg:"+err);
+		}
+	}
+
+	void InGame(){
+		if (GUILayout.Button ("Quit")) {
+			Network.Disconnect();
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
